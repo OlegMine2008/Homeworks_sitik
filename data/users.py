7981c5eda78853530e71720fa3a81acb47a5312c
OlegMine2008 <olegkosh08@gmail.com>
2026-04-12 10:28:25 +0300

    as

---
+++ data/users.py
@@ -0,0 +1,20 @@
+import datetime
+import sqlalchemy
+
+from .db_session import SqlAlchemyBase
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True,
+                           autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              unique=True,
+                              index=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+
