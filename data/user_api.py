c1201f16e0079ba75531bc504938a3ef9f2a5a8b
OlegMine2008 <olegkosh08@gmail.com>
2026-04-27 17:46:49 +0300

    Почти готов к дедлайну

--- data/user_api.py
+++ data/user_api.py
@@ -1,17 +1,19 @@
 import flask
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 from . import db_session
 from users import User
 from register_form import RegisterForm
+from login_form import LoginForm
 
-blueprint = flask.Blueprint(
+user_blueprint = flask.Blueprint(
     'user_api',
     __name__,
     template_folder='templates'
 )
 
 
-@blueprint.route('/register', methods=['GET', 'POST'])
+@user_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
@@ -32,3 +34,23 @@ def register():
         db_sess.commit()
         return flask.redirect('/login')
     return flask.render_template('register.html', title='Регистрация', form=form)
+
+@user_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return flask.redirect("/")
+        return flask.render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return flask.render_template('login.html', title='Авторизация', form=form)
+
+@user_blueprint.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return flask.redirect("/")
--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 
 from .data.db_session import global_init, create_session
 from .data.users import User
+from .data.user_api import user_blueprint
+
+
+app = Flask(__name__)
+app.register_blueprint(user_blueprint)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+# Основное
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = create_session()
+    return db_sess.query(User).get(user_id)
+
+@app.route('/')
+def index():
+    db_sess = create_session()
+    users = db_sess.query(User).all()
+    names = {name.id: (name.surname, name.name) for name in users}
+    return render_template('index.html', names=names, title='Work log')
+
+
+def main():
+    global_init('bd/homework_site.sqlite')
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
